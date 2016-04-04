73d0b0da288aa83496d4c14138a80705a332a0c3
Riccardo <pulver22@gmail.com>
2016-04-04 18:09:02 +0100

    Added twist messages

--- src/flight.cpp
+++ src/flight.cpp
@@ -8,13 +8,18 @@
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
 #include <ardrone_autonomy/Navdata.h>
+#include <geometry_msgs/Twist.h>
 #include <iostream>
+
+uint32_t state;
+geometry_msgs::Twist twist_msg;
+geometry_msgs::Twist twist_msg_hover;
 std_msgs::Empty emp_msg;
 
 using namespace std;
 
 void getStateCallBack (const ardrone_autonomy::NavdataConstPtr &msg);
-uint32_t state;
+
 
 int main (int argc, char **argv)
 {
@@ -22,49 +27,88 @@ int main (int argc, char **argv)
 	ros::init (argc, argv, "ARDrone_test");
 	ros::NodeHandle node;
 	ros::Rate loop_rate (50);
-	ros::Publisher pub_take_off, pub_land;
+	ros::Publisher pub_empty_take_off;
+	ros::Publisher pub_empty_land;
+	ros::Publisher pub_twist;
+	ros::Publisher pub_empty_reset;
 	ros::Subscriber sub_drone_state;
-	pub_land = node.advertise<std_msgs::Empty> ("/ardrone/land", 1);  /* Message queue length is just 1 */
-	pub_take_off = node.advertise<std_msgs::Empty> ("/ardrone/takeoff", 1);  /* Message queue length is just 1 */
+	pub_empty_reset = node.advertise<std_msgs::Empty> ("/ardrone/reset", 1);
+	pub_twist = node.advertise<geometry_msgs::Twist> ("/cmd_vel", 1);
+	pub_empty_land = node.advertise<std_msgs::Empty> ("/ardrone/land", 1);
+	pub_empty_take_off = node.advertise<std_msgs::Empty> ("/ardrone/takeoff", 1);
 	sub_drone_state = node.subscribe<ardrone_autonomy::Navdata> ("/ardrone/navdata", 1, getStateCallBack);
-	double time_start, offset;
+
+	
+	//hover message
+	twist_msg_hover.linear.x=0.0; 
+	twist_msg_hover.linear.y=0.0;
+	twist_msg_hover.linear.z=0.0;
+	twist_msg_hover.angular.x=0.0; 
+	twist_msg_hover.angular.y=0.0;
+	twist_msg_hover.angular.z=0.0;  
 
 
 	while (ros::ok()) {
-			/*
-			pub_empty.publish (emp_msg);   // launches the drone
-			ros::spinOnce();
-			loop_rate.sleep();
-			time_start = (double) ros::Time::now().toSec();
-			offset = (double) ros::Duration (2,0).toSec();
-
-			//ROS_INFO ("ARdrone launched");
-
-			cout << "Tempo attuale: "<< (double) ros::Time::now().toSec() << endl;
-			cout << "Tempo finale programato: "<< time_start << " + "<< offset << " = " << time_start + offset << endl;
-
-			if ( (double) ros::Time::now().toSec() >= time_start + offset) {
-			        //ros::shutdown();
-			        cout << "spegni" <<endl;
-			    }
-
-			*/
-
 			switch (state) {
-				case (2) :
-					pub_take_off.publish (emp_msg); /* launches the drone */
+				case (2) : /* if the drone is landed on the ground*/
+					pub_empty_take_off.publish (emp_msg); /* launches the drone */
+					sleep (2);
 					break;
-				
-				
-				case(3):
-					sleep(5);
-					pub_land.publish<>(emp_msg);
-					exit(0);
-			}	
+
+
+				case (3) : /*if the drone is flying*/
+					/* Move along x axis */
+					twist_msg.linear.x=1.0;
+					twist_msg.linear.y=0.0;
+					twist_msg.linear.z=0.0;
+					twist_msg.angular.x=0.0;
+					twist_msg.angular.y=0.0;
+					twist_msg.angular.z=0.0;
+					pub_twist.publish (twist_msg); 
+					sleep (3);
+					
+					/* Hover */
+					pub_twist.publish (twist_msg_hover); 
+					sleep (3);
+					
+					/* Move along y */
+					twist_msg.linear.x=0.0;
+					twist_msg.linear.y=0.3;
+					twist_msg.linear.z=0.0;
+					twist_msg.angular.x=0.0;
+					twist_msg.angular.y=0.0;
+					twist_msg.angular.z=0.0;
+					pub_twist.publish (twist_msg); 
+					sleep (3);
+					
+					/* Hover */
+					pub_twist.publish (twist_msg_hover); 
+					sleep (3);
+					
+					/* Rotate along z */
+					twist_msg.linear.x=0.0;
+					twist_msg.linear.y=0.0;
+					twist_msg.linear.z=0.0;
+					twist_msg.angular.x=0.0;
+					twist_msg.angular.y=0.0;
+					twist_msg.angular.z=0.4;
+					pub_twist.publish (twist_msg); 
+					sleep (4);
+					
+					/* Hover */
+					pub_twist.publish (twist_msg_hover); 
+					sleep (3);
+					
+					/* Land */
+					pub_empty_land.publish (emp_msg); 
+					exit (0);
+				}
+
 			ros::spinOnce();
 			loop_rate.sleep();
-		} //ros::ok loop
-}//main
+		}
+}
+
 
 void getStateCallBack (const ardrone_autonomy::NavdataConstPtr &msg)
 {
@@ -80,7 +124,7 @@ void getStateCallBack (const ardrone_autonomy::NavdataConstPtr &msg)
 			break;
 
 		case (4) :
-			ROS_INFO ("Hoovering");
+			ROS_INFO ("Hovering");
 			break;
 
 		case (6) :
